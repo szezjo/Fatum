6a6ed4a4d6b44fb8f3441235f15528bc7f6ea283
szezjo <szezjo@gmail.com>
2020-08-28 17:44:08 +0200

    Some tests added

--- fatum.c
+++ fatum.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "fatum.h"
 
 int main() {
-    printf("hello fatum\n");
+    FILE *f;
+    f = fopen("fat16.bin","rb");
+    boot_t br;
+    int ret = fread(&br,sizeof(br),1,f);
+    fclose(f);
+    assert(ret==1);
+
+    printf("Assembly code: %02hhx %02hhx %02hhx\n",br.assembly_code[0],br.assembly_code[1],br.assembly_code[2]);
+    printf("OEM: %.*s\n",8,br.oem);
+    printf("Bytes per sector: %hd\n",br.bytes_per_sector);
+    printf("Sectors per cluster: %hhu\n",br.sectors_per_cluster);
+    printf("Reserved area size: %hu\n",br.reserved_area_size);
+    printf("Number of FATs: %hhu\n",br.number_of_fats);
+    printf("Max files in root: %hu\n",br.max_files_in_root);
+    printf("Sectors in FS: %hu\n",br.sectors_in_fs);
+    printf("Media type: 0x%02hhx\n",br.media_type);
+    printf("Size of FAT: %hu\n",br.size_of_fat);
+    printf("Sectors per track: %hu\n",br.sectors_per_track);
+    printf("Heads: %hu\n",br.heads);
+    printf("Sectors before start: %u\n",br.sectors_before_start);
+    printf("Sectors in FS (large): %u\n",br.sectors_in_fs_large);
+    printf("Drive number: 0x%02hhx\n",br.drive_number);
+    printf("Boot signature: 0x%02hhx\n",br.boot_signature);
+    printf("Volume serial number: %u\n",br.vol_serial_number);
+    printf("Volume label: %.*s\n",11,br.vol_label);
+    printf("FS type label: %.*s\n",8,br.fs_type);
+    printf("Signature value: 0x%04hx\n",br.signature_value);
+
+    
+
     return 0;
 }
 
--- fatum.h
+++ fatum.h
@@ -16,15 +16,15 @@ typedef struct __attribute__ ((__packed__)) boot {
     char assembly_code[3]; // instructions to jump to boot code
     char oem[8]; // in ASCII
     short bytes_per_sector; // 512,1024,2048,4096
-    char sectors_per_cluster; // must be a power of 2 and cluster size must be <=32 KB
-    short reserved_area_size; // in sectors
-    char number_of_fats; // usually 2
-    short max_files_in_root; // 0 for FAT32
-    short sectors_in_fs; // if 2B is not large enough, set to 0 and user 4B value in bytes 32-35
+    unsigned char sectors_per_cluster; // must be a power of 2 and cluster size must be <=32 KB
+    unsigned short reserved_area_size; // in sectors
+    unsigned char number_of_fats; // usually 2
+    unsigned short max_files_in_root; // 0 for FAT32
+    unsigned short sectors_in_fs; // if 2B is not large enough, set to 0 and user 4B value in bytes 32-35
     char media_type; // 0xf0 = removable, 0xf8 = fixed
-    short size_of_fat; // in sectors, 0 for FAT32
-    short sectors_per_track;
-    short heads;
+    unsigned short size_of_fat; // in sectors, 0 for FAT32
+    unsigned short sectors_per_track;
+    unsigned short heads;
     uint32_t sectors_before_start; // before the start partition
     uint32_t sectors_in_fs_large;
     char drive_number; // BIOS INT 13h (low level disk services) drive number
@@ -32,7 +32,7 @@ typedef struct __attribute__ ((__packed__)) boot {
     char boot_signature; // to validate next three fields (0x29)
     uint32_t vol_serial_number;
     char vol_label[11]; // in ASCII
-    char fs_type_level[8]; // in ASCII
+    char fs_type[8]; // in ASCII
     char not_used_02[448];
     short signature_value; // 0xaa55
 } boot_t;
